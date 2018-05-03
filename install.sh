6b2a38efda965afa10096e54928b13a6e37ecbe2
bstrie <ben.striegel@gmail.com>
2018-05-03 22:19:24 +0000

    irssi

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ echo "\nsource ~/.dotfiles/.benrc" >> ~/.bashrc
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.irssi ~/.irssi
+ln -s ~/.dotfiles/.inputrc ~/.inputrc
