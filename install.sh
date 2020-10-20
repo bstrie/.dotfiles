b1d405208bb8e4b6f3ca58071c57c4b56ebae82d
bstrie <bstrie@thinwsl>
2020-10-20 15:42:57 -0400

    Remove irssi config
    Farewell, sweet prince

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ echo "\nsource ~/.dotfiles/.benrc" >> ~/.bashrc
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/.irssi ~/.irssi
 ln -s ~/.dotfiles/.inputrc ~/.inputrc
