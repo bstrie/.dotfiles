09f47a6c5c55f6e60d59c07cf64b63f9f167e33c
bstrie <865233+bstrie@users.noreply.github.com>
2021-05-01 18:17:49 -0400

    Alacritty config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.inputrc ~/.inputrc
+ln -s ~/.dotfiles/alacritty.yml ~/.config/alacritty.yml
